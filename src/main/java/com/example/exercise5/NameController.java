573f1151c1e1f563d1f303c569c7479a6e00fdba
Federico Maiello <federicomaiello@hotmail.it>
2024-03-12 12:20:24 +0100

    invertName

--- src/main/java/com/example/exercise5/NameController.java
+++ src/main/java/com/example/exercise5/NameController.java
@@ -1,16 +1,31 @@
 package com.example.exercise5;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/api")
 public class NameController {
+    @Operation(summary = "Get Name", description = "Get the provided name")
     @GetMapping("/name")
+    @ApiResponses (value = {
+            @ApiResponse(responseCode = "200", description = "ok"),
+            @ApiResponse(responseCode = "400", description = "bad request")
+    })
     public String getName(@RequestParam String name){
         return name;
     }
+
+
+    @Operation(summary = "Invert Name", description = "Invert the provided name")
     @PostMapping("/invertName")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ok"),
+            @ApiResponse(responseCode = "400", description = "bad request")
+    })
     public String invertName(@RequestParam String name){
         StringBuilder invertName = new StringBuilder();
         for(int i = name.length() - 1; i>0; i--){
